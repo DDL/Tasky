99c9ca4d256b20860abc144d65015a32b6d67377
DDL <DDL@users.noreply.github.com>
2024-03-06 22:34:24 +0100

    delete todo

--- app/src/main/java/com/ddoczi/tasky/core/presentation/MainActivity.kt
+++ app/src/main/java/com/ddoczi/tasky/core/presentation/MainActivity.kt
@@ -25,9 +25,9 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private val viewModel: MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // TODO Splash screen
         setContent {
             TaskyTheme {
                 val navController = rememberNavController()
