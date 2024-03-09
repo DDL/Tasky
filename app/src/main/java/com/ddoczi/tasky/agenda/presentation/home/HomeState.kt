baf832c6c4c5783bd44373f4104947ba7d5bfe47
DDL <DDL@users.noreply.github.com>
2024-03-09 17:36:19 +0100

    delete lines to fit in 400 lines

--- app/src/main/java/com/ddoczi/tasky/agenda/presentation/home/HomeState.kt
+++ app/src/main/java/com/ddoczi/tasky/agenda/presentation/home/HomeState.kt
@@ -3,10 +3,4 @@ package com.ddoczi.tasky.agenda.presentation.home
 data class HomeState(
     val isLoggedOut: Boolean = false,
     val showLogout: Boolean = false,
-    val agendaTypes: List<HomeAgendaType> = listOf(
-        HomeAgendaType.Event,
-        HomeAgendaType.Task,
-        HomeAgendaType.Reminder
-    ),
-    val agendaItems: List<Unit> = emptyList(),
 )
